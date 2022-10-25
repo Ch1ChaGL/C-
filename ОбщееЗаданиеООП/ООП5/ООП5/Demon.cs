f2f988a8d7abbdfee66a967c1f262c6ea056551d
Danil Markov <102900110+Ch1ChaGL@users.noreply.github.com>
2022-10-25 18:28:55 +0300

    Add files via upload

---
+++ ОбщееЗаданиеООП/ООП5/ООП5/Demon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ООП5
+{
+    internal class Demon : Entity
+    {
+
+        private int intelligence;
+        public Demon(string name, int power, int health, int intelligence) : base(name, power, health)
+        {
+            this.intelligence = intelligence;
+        }
+        public override void Show()
+        {
+            base.Show();
+            Console.WriteLine("Интелект = " + intelligence);
+        }
+
+        public int GetIntelligence()
+        {   
+            return intelligence;
+        }
+    }
+}
