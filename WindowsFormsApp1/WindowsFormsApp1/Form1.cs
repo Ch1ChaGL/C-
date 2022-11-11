54ea0aa3279326c9bc5edf26efa1a000c5aaac02
Danil Markov <102900110+Ch1ChaGL@users.noreply.github.com>
2022-11-11 23:46:41 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            double max = 30.0;
+            double min = 1.0;
+
+            double Height = random.NextDouble() * (max - min) + min;
+            double Width = random.NextDouble() * (max - min) + min;
+            Height = Math.Round(Height, 1);
+            Width = Math.Round(Width, 1);
+            double S = Height * Width;
+            String answer = "Площадь прямоугольника со сторонами " + Height + " X " + Width + "см равна " + S + "кв.см";
+            label1.Text= answer;
+        }
+    }
+}
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int count = 1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int YButton = button1.Top;
+            int X = label1.Left;
+            int Y = label1.Top;
+            Y += 5;
+            label1.Location = new Point(X, Y);
+            if (count % 2 == 0)
+            {
+                label1.ForeColor = Color.Red;
+            }
+            else
+            {
+                label1.ForeColor = Color.Blue;
+            }
+            label1.Text = "Меня вывели " + count + " раз";
+            count++;
+            if (Y >= YButton)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
