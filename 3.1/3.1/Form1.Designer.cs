16ff581bfe6cd1211c550a99da3b986e54164c67
Danil Markov <102900110+Ch1ChaGL@users.noreply.github.com>
2022-11-15 20:03:39 +0300

    Add files via upload

---
+++ 3.1/3.1/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace _3._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 81);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.ClientSizeChanged += new System.EventHandler(this.button1_ClientSizeChanged);
+            this.button1.CursorChanged += new System.EventHandler(this.button1_CursorChanged);
+            this.button1.DragDrop += new System.Windows.Forms.DragEventHandler(this.button1_DragDrop);
+            this.button1.MouseCaptureChanged += new System.EventHandler(this.button1_MouseCaptureChanged);
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button1_MouseUp);
+            this.button1.Resize += new System.EventHandler(this.button1_Resize);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 3.1/3.1/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            //    button1.Width = Cursor.Position.X;
+            //    button1.Height = Cursor.Position.Y;
+            //int width = Cursor.Position.X;
+            //int height = Cursor.Position.Y;
+            //button1.SetBounds(0, 0, width, height);
+
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+
+            //Cursor.Position = new Point(button1.Width, button1.Height);
+            //int width = Cursor.Position.X;
+            //int height = Cursor.Position.Y;
+            //button1.SetBounds(0, 0, width, height);
+           
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Location = new Point(0,0);
+
+        }
+
+        private void button1_Resize(object sender, EventArgs e)
+        {
+            //Cursor.Position = new Point(button1.Width, button1.Height);
+            
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+           
+        }
+
+        private void button1_ClientSizeChanged(object sender, EventArgs e)
+        {
+
+          
+        }
+
+        private void button1_CursorChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_MouseCaptureChanged(object sender, EventArgs e)
+        {//
+           // button1.Width = Cursor.Position.X;
+           // button1.Height = Cursor.Position.Y;
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //button1.Width = Cursor.Position.X;
+           // button1.Height = Cursor.Position.Y;
+        }
+
+        private void button1_DragDrop(object sender, DragEventArgs e)
+        {
+          
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+           
+            button1.Width = Cursor.Position.X;
+            button1.Height = Cursor.Position.Y;
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
