c1f98054702116ba5b980ed54c1f63b270559bbd
Danil Markov <102900110+Ch1ChaGL@users.noreply.github.com>
2022-11-24 20:35:24 +0300

    Add files via upload

---
+++ 7.3/7.3/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace _7._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.настройкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ичнаяToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ичнаяToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ричнаяToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ричнаяToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(180, 324);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 57);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(55, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 18);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Резултат";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(58, 168);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(362, 24);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(54, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(366, 24);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Введите число в 10 системе счисления";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.настройкиToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(546, 24);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ичнаяToolStripMenuItem,
+            this.ичнаяToolStripMenuItem1,
+            this.ричнаяToolStripMenuItem,
+            this.ричнаяToolStripMenuItem1});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(131, 20);
+            this.toolStripMenuItem1.Text = "Системы счисления";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click_1);
+            // 
+            // настройкиToolStripMenuItem
+            // 
+            this.настройкиToolStripMenuItem.Enabled = false;
+            this.настройкиToolStripMenuItem.Name = "настройкиToolStripMenuItem";
+            this.настройкиToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.настройкиToolStripMenuItem.Text = "Настройки";
+            // 
+            // ичнаяToolStripMenuItem
+            // 
+            this.ичнаяToolStripMenuItem.Name = "ичнаяToolStripMenuItem";
+            this.ичнаяToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.ичнаяToolStripMenuItem.Text = "2-ичная";
+            this.ичнаяToolStripMenuItem.Click += new System.EventHandler(this.toolStripMenuItem1_Click_1);
+            // 
+            // ичнаяToolStripMenuItem1
+            // 
+            this.ичнаяToolStripMenuItem1.Name = "ичнаяToolStripMenuItem1";
+            this.ичнаяToolStripMenuItem1.Size = new System.Drawing.Size(131, 22);
+            this.ичнаяToolStripMenuItem1.Text = "8-ричная";
+            this.ичнаяToolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click_1);
+            // 
+            // ричнаяToolStripMenuItem
+            // 
+            this.ричнаяToolStripMenuItem.Name = "ричнаяToolStripMenuItem";
+            this.ричнаяToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.ричнаяToolStripMenuItem.Text = "10-ричная";
+            this.ричнаяToolStripMenuItem.Click += new System.EventHandler(this.toolStripMenuItem1_Click_1);
+            // 
+            // ричнаяToolStripMenuItem1
+            // 
+            this.ричнаяToolStripMenuItem1.Name = "ричнаяToolStripMenuItem1";
+            this.ричнаяToolStripMenuItem1.Size = new System.Drawing.Size(131, 22);
+            this.ричнаяToolStripMenuItem1.Text = "16-ричная";
+            this.ричнаяToolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click_1);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripButton4});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(546, 25);
+            this.toolStrip1.TabIndex = 14;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::_7._3.Properties.Resources._10;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = global::_7._3.Properties.Resources._8;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = global::_7._3.Properties.Resources._2;
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "toolStripButton3";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = global::_7._3.Properties.Resources._16;
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton4.Text = "toolStripButton4";
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "«Расчет в различных  системах>";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ичнаяToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ичнаяToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem настройкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ричнаяToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ричнаяToolStripMenuItem1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+    }
+}
+
---
+++ 7.3/7.3/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7._3
+{
+    public partial class Form1 : Form
+    {
+
+        Boolean format10 = true;
+        Boolean format2 = false;
+        Boolean format8 = false;
+        Boolean format16 = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int format2n(int num1, int num2)
+        {
+            if (num1 == 1 && num2 == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private void toolStripMenuItem1_Click_1(object sender, EventArgs e)
+        {
+            ToolStripMenuItem Button = (ToolStripMenuItem)sender;
+            if (Button.Text == "10-ричная")
+            {
+                format10 = true;
+                format2 = false;
+                format8 = false;
+                format16 = false;
+                label1.Text = "Введите текст в 10 системе";
+                textBox1.Clear();
+                label2.Text = "";
+
+            }
+            if (Button.Text == "8-ричная")
+            {
+                format10 = false;
+                format2 = false;
+                format8 = true;
+                format16 = false;
+                label1.Text = "Введите текст в 8 системе";
+                textBox1.Clear();
+                label2.Text = "";
+            }
+            if (Button.Text == "2-ичная")
+            {
+                format10 = false;
+                format2 = true;
+                format8 = false;
+                format16 = false;
+                label1.Text = "Введите текст в 2 системе";
+                textBox1.Clear();
+                label2.Text = "";
+            }
+            if (Button.Text == "16-ричная")
+            {
+                format10 = false;
+                format2 = false;
+                format8 = false;
+                format16 = true;
+                label1.Text = "Введите текст в 16 системе";
+                textBox1.Clear();
+                label2.Text = "";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (format10)
+            {
+                char[] num1 = new char[textBox1.Text.Length];
+                num1 = textBox1.Text.ToCharArray();
+                if (num1.Length < 2)
+                {
+                    label2.Text = "Число не подподает под задание";
+                }
+                else
+                {
+                    int answer4 = (Convert.ToInt32(num1[0]) - 48) * Convert.ToInt32(num1[1] - 48);
+                    int answer3 = (Convert.ToInt32(num1[0]) - 48) * Convert.ToInt32(num1[num1.Length - 1] - 48);
+                    int answer2 = (Convert.ToInt32(num1[num1.Length - 1]) - 48) * Convert.ToInt32(num1[num1.Length - 2] - 48);
+                    label2.Text = $"Резултат произведения первого и последнего числа равна {answer3} \nРезултат произведения первого и второго числа равна {answer4} \nРезультат произведения последних двух цифр {answer2}";
+                }
+            }
+            if (format2)
+            {
+                int answer, answer2, answer3, answer4;
+                char[] num1 = new char[textBox1.Text.Length];
+                num1 = textBox1.Text.ToCharArray();
+                if (num1.Length < 2)
+                {
+                    label2.Text = "Число не подподает под задание";
+                }
+                else
+                {
+                    answer = format2n(num1[0] - 48, num1[textBox1.Text.Length - 1] - 48);
+                    answer2 = format2n(num1[0] - 48, num1[1] - 48);
+                    answer3 = format2n(num1[textBox1.Text.Length - 1] - 48, num1[textBox1.Text.Length - 2] - 48);
+                    label2.Text = $"Произведение первого и последнего числа в двоичной системе\nравна {answer}\n" +
+                        $"Произведение первых двух чисул в двоичной системе\nравна {answer2}\n" +
+                        $"Произведение последних двух чисул в двоичной системе\nравна {answer3}\n";
+                }
+
+            }
+            if (format8)
+            {
+                int answer, answer2, answer3, answer4;
+                char[] num1 = new char[textBox1.Text.Length];
+                num1 = textBox1.Text.ToCharArray();
+                if (num1.Length < 2)
+                {
+                    label2.Text = "Число не подподает под задание";
+                }
+                else
+                {
+                    answer = Convert.ToInt32(num1[0] - 48) * Convert.ToInt32(num1[textBox1.Text.Length - 1] - 48);
+                    answer2 = Convert.ToInt32(num1[0] - 48) * Convert.ToInt32(num1[1] - 48);
+                    answer3 = Convert.ToInt32(num1[textBox1.Text.Length - 1] - 48) * Convert.ToInt32(num1[textBox1.Text.Length - 2] - 48);
+                    label2.Text = $"Резултат произведения первого и последнего числа равна {Convert.ToString(answer, 8)} \nРезултат произведения первого и второго числа равна {Convert.ToString(answer2, 8)} \nРезультат произведения последних двух цифр {Convert.ToString(answer3, 8)}";
+                }
+
+
+            }
+            if (format16)
+            {
+                int answer, answer2, answer3, answer4;
+                char[] num1 = new char[textBox1.Text.Length];
+                num1 = textBox1.Text.ToCharArray();
+                if (num1.Length < 2)
+                {
+                    label2.Text = "Число не подподает под задание";
+                }
+                else
+                {
+                    if (num1[0] - 48 > 15)
+                    {
+                        answer = num1[0] - 55;
+                    }
+                    else
+                    {
+                        answer = Convert.ToInt32(num1[0] - 48);
+                    }
+                    if (num1[1] - 48 > 15)
+                    {
+                        answer2 = num1[1] - 55;
+                    }
+                    else
+                    {
+                        answer2 = Convert.ToInt32(num1[1] - 48);
+                    }
+                    if (num1[textBox1.Text.Length - 1] - 48 > 15)
+                    {
+                        answer3 = num1[textBox1.Text.Length - 1] - 55;
+                    }
+                    else
+                    {
+                        answer3 = Convert.ToInt32(num1[textBox1.Text.Length - 1] - 48);
+                    }
+                    if (num1[textBox1.Text.Length - 2] - 48 > 15)
+                    {
+                        answer4 = num1[textBox1.Text.Length - 2] - 55;
+                    }
+                    else
+                    {
+                        answer4 = Convert.ToInt32(num1[textBox1.Text.Length - 2] - 48);
+                    }
+
+                    label2.Text = $"Произведение 1 и последнего числа = {Convert.ToString(answer * answer3, 16)} \n" +
+                        $"Произведение 1 и 2 числа = {Convert.ToString(answer * answer2, 16)} \n" +
+                        $"Произведение двух последних чисел = {Convert.ToString(answer4 * answer3, 16)} \n";
+
+
+
+
+
+
+
+                }
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (format10)
+            {
+                if (!(e.KeyChar >= '0' && e.KeyChar <= '9') && e.KeyChar != '\b')
+                {
+                    e.Handled = true;
+                }
+            }
+            if (format2)
+            {
+                if (!(e.KeyChar >= '0' && e.KeyChar <= '1') && e.KeyChar != '\b')
+                {
+                    e.Handled = true;
+                }
+            }
+            if (format8)
+            {
+                if (!(e.KeyChar >= '0' && e.KeyChar <= '7') && e.KeyChar != '\b')
+                {
+                    e.Handled = true;
+                }
+            }
+            if (format16)
+            {
+                if (!(e.KeyChar >= '0' && e.KeyChar <= '9') && !(e.KeyChar >= 'A' && e.KeyChar <= 'F') && e.KeyChar != '\b')
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            ToolStripButton Button = (ToolStripButton)sender;
+            if (Button.Name == "toolStripButton1")
+            {
+                format10 = true;
+                format2 = false;
+                format8 = false;
+                format16 = false;
+                label1.Text = "Введите текст в 10 системе";
+                textBox1.Clear();
+                label2.Text = "";
+
+            }
+            if (Button.Name == "toolStripButton2")
+            {
+                format10 = false;
+                format2 = false;
+                format8 = true;
+                format16 = false;
+                label1.Text = "Введите текст в 8 системе";
+                textBox1.Clear();
+                label2.Text = "";
+            }
+            if (Button.Name == "toolStripButton3")
+            {
+                format10 = false;
+                format2 = true;
+                format8 = false;
+                format16 = false;
+                label1.Text = "Введите текст в 2 системе";
+                textBox1.Clear();
+                label2.Text = "";
+            }
+            if (Button.Name == "toolStripButton4")
+            {
+                format10 = false;
+                format2 = false;
+                format8 = false;
+                format16 = true;
+                label1.Text = "Введите текст в 16 системе";
+                textBox1.Clear();
+                label2.Text = "";
+            }
+        }
+    }
+}
