aaa39fdb776b918d6780896125d363f377fff381
Danil Markov <102900110+Ch1ChaGL@users.noreply.github.com>
2023-05-25 23:33:56 +0300

    Add files via upload

---
+++ 2_Задание_16Вариант/2_Задание_16Вариант/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace _2_Задание_16Вариант
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.system = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NaturalNumber = new System.Windows.Forms.TextBox();
+            this.translate = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.translateFromFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.result = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.result)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // system
+            // 
+            this.system.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.system.FormattingEnabled = true;
+            this.system.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16"});
+            this.system.Location = new System.Drawing.Point(21, 96);
+            this.system.Name = "system";
+            this.system.Size = new System.Drawing.Size(221, 32);
+            this.system.TabIndex = 0;
+            this.system.TextChanged += new System.EventHandler(this.NaturalNumber_TextChanged);
+            this.system.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.system_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(17, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Целевая система счисления";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(17, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Натуральное число";
+            // 
+            // NaturalNumber
+            // 
+            this.NaturalNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NaturalNumber.Location = new System.Drawing.Point(21, 204);
+            this.NaturalNumber.Name = "NaturalNumber";
+            this.NaturalNumber.Size = new System.Drawing.Size(180, 29);
+            this.NaturalNumber.TabIndex = 3;
+            this.NaturalNumber.TextChanged += new System.EventHandler(this.NaturalNumber_TextChanged);
+            this.NaturalNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NaturalNumber_KeyPress);
+            // 
+            // translate
+            // 
+            this.translate.Enabled = false;
+            this.translate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.translate.Location = new System.Drawing.Point(21, 272);
+            this.translate.Name = "translate";
+            this.translate.Size = new System.Drawing.Size(144, 46);
+            this.translate.TabIndex = 4;
+            this.translate.Text = "Перевести";
+            this.translate.UseVisualStyleBackColor = true;
+            this.translate.Click += new System.EventHandler(this.translate_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.translateFromFile});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // translateFromFile
+            // 
+            this.translateFromFile.Enabled = false;
+            this.translateFromFile.Name = "translateFromFile";
+            this.translateFromFile.Size = new System.Drawing.Size(119, 20);
+            this.translateFromFile.Text = "Перевод из файла";
+            this.translateFromFile.Click += new System.EventHandler(this.translateFromFile_Click);
+            // 
+            // result
+            // 
+            this.result.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.result.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.result.ColumnHeadersVisible = false;
+            this.result.Location = new System.Drawing.Point(354, 204);
+            this.result.Name = "result";
+            this.result.RowHeadersVisible = false;
+            this.result.Size = new System.Drawing.Size(402, 31);
+            this.result.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.translate);
+            this.Controls.Add(this.NaturalNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.system);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Перевод натурального числа в n-ую систему счисления";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.result)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox system;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NaturalNumber;
+        private System.Windows.Forms.Button translate;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem translateFromFile;
+        private System.Windows.Forms.DataGridView result;
+    }
+}
+
---
+++ 2_Задание_16Вариант/2_Задание_16Вариант/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_Задание_16Вариант
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int naturalNumber;
+        int targetSystem;
+        private void NaturalNumber_TextChanged(object sender, EventArgs e)
+        {
+            if(system.Text == "") translateFromFile.Enabled = false;
+            else translateFromFile.Enabled = true;
+            if (system.Text == "" || NaturalNumber.Text == "")
+            {
+                translate.Enabled = false;
+            }
+            else
+            {
+                translate.Enabled = true;
+            }
+        }
+
+        private void NaturalNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+      
+            if (e.KeyChar == '0' && NaturalNumber.Text.Length < 1)
+            {
+                MessageBox.Show("Натуральное число не может начинаться с нуля");
+                e.Handled = true;
+            }
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void translate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                result.Rows.Clear();
+                naturalNumber = int.Parse(NaturalNumber.Text);
+                targetSystem = int.Parse(system.Text);
+                result.RowCount = 1;
+                result.ColumnCount = 1;
+                result.Rows[0].Cells[0].Value = translateToTargetSystem(naturalNumber, targetSystem);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка {ex.Message}");
+            }
+        }
+
+        private void system_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private string translateToTargetSystem(int natural, int target)
+        {
+            if (target < 2 || target > 36)
+            {
+                throw new ArgumentException("Недопустимое основание системы счисления.");
+            }
+
+            if (natural == 0)
+            {
+                return "0";
+            }
+
+            string convertedNumber = string.Empty;
+            while (natural > 0)
+            {
+                int remainder = natural % target;
+                char digit = (char)(remainder < 10 ? remainder + '0' : remainder - 10 + 'A');
+                convertedNumber = digit + convertedNumber;
+                natural /= target;
+            }
+
+            return convertedNumber;
+        }
+
+        private void translateFromFile_Click(object sender, EventArgs e)
+        {
+            targetSystem = int.Parse(system.Text);
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Выберите файл";
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                List<int> numbers = new List<int>();
+
+                try
+                {
+                    string[] lines = File.ReadAllLines(filePath);
+
+                    foreach (string line in lines)
+                    {
+                        if (int.TryParse(line, out int number))
+                        {
+                            numbers.Add(number);
+                        }
+                    }
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine("Ошибка при чтении файла: " + ex.Message);
+                }
+                List<string> targetNums = new List<string>();
+                foreach (int number in numbers) 
+                {
+                    targetNums.Add(translateToTargetSystem(number, targetSystem));
+                }
+                result.Rows.Clear();
+                result.ColumnCount = targetNums.Count;
+                
+                for (int i = 0; i < targetNums.Count; i++)
+                {
+                    result.Rows[0].Cells[i].Value = targetNums[i];
+                }
+            }
+        }
+    }
+}
